8fa09df15f17f0663acf711ddb7ecc853d0db123
Onojeta Brown <onosbrown.saved@gmail.com>
2024-11-03 18:58:48 +0100

    added setup command

---
+++ Modules/Result/Console/Setup.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Modules\Result\Console;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Storage;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
+
+class Setup extends Command
+{
+
+    protected $name = 'app:setup';
+
+    public function handle()
+    {
+        $ac = env('ACCES_CODE', '');
+        $email = env('ADMIN_EMAIL', 'onosbrown.saved@gmail.com');
+        $password = env('ADMIN_PASSWORD', '#1414bruno#');
+
+        Storage::put('.app_installed', $ac);
+        Storage::put('.user_email', $email);
+        Storage::put('.user_pass', $password);
+
+        $this->info('Setup completed');
+    }
+}
