09b2b1843a22a99f5bd2e24237cf6313b3a9a9ec
Onojeta Brown <onosbrown.saved@gmail.com>
2025-01-14 12:47:29 +0100

    deploy

--- Modules/Result/Traits/ResultTrait.php
+++ Modules/Result/Traits/ResultTrait.php
@@ -152,7 +152,7 @@ trait ResultTrait
             $marks = $marks_data->pluck('total_marks', 'exam_title')->toArray();
             $grade = $this->getGrade($sum, $student->type);
             $obj = $objectives->firstWhere('subject_code', $marks_data[0]->subject_code);
-       
+
             $rows[] = (object)[
                 'subject' => $subject_name,
                 'objectives' => array_map('trim', explode('|', $obj['text'] ?? '')),
@@ -187,7 +187,7 @@ trait ResultTrait
             'remark' => $result_data->remark,
             'exam_type' => $type,
         ];
-
+        dd($data);
         return $data;
     }
 
